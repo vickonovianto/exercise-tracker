4440f93ce079b833da4fe59a40d4ca0f35a81483
Vicko Novianto <vicko1011@gmail.com>
2022-12-29 20:21:28 +0700

    add mongoose package, add mongo uri to sample.env

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 require('dotenv').config()
+
 
 app.use(cors())
 app.use(express.static('public'))
