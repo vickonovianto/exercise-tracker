ca5edf3edf6c97909ca5e206707ba3d0f742d188
Vicko Novianto <vicko1011@gmail.com>
2022-12-30 15:36:23 +0700

    handle creation of new exercise

--- index.js
+++ index.js
@@ -34,6 +34,33 @@ const userSchema = new Schema({
 const Exercise = mongoose.model("Exercise", exerciseSchema)
 const User = mongoose.model("User", userSchema)
 
+// Validates that the input string is a valid date formatted as "yyyy-mm-dd"
+function isValidDate(dateString)
+{
+    // First check for the pattern
+    if(!/^\d{4}-\d{2}-\d{2}$/.test(dateString))
+        return false;
+
+    // Parse the date parts to integers
+    const parts = dateString.split("-");
+    const year = parseInt(parts[0], 10);
+    const month = parseInt(parts[1], 10);
+    const day = parseInt(parts[2], 10);
+
+    // Check the ranges of month and year
+    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+        return false;
+
+    const monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+    // Adjust for leap years
+    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+        monthLength[1] = 29;
+
+    // Check the range of the day
+    return day > 0 && day <= monthLength[month - 1];
+};
+
 app.use(cors())
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -45,11 +72,11 @@ app.get('/', (req, res) => {
 app.post('/api/users', (req, res) => {
   const username = req.body.username.trim() // get username and trim space from start and end from request body
   const user = new User({ username: username })
-  user.save((err, savedUser) => {
+  user.save((err, createdUser) => {
     if (err) {
       res.json({error: err.message })
     } else {
-      res.json({ username: savedUser.username, _id: savedUser.id })
+      res.json({ username: createdUser.username, _id: createdUser.id })
     }
   })
 }) 
@@ -66,6 +93,45 @@ app.get('/api/users', (req, res) => {
     }
   })
 })
+
+// handle creation of new exercise of a certain user
+app.post('/api/users/:_id/exercises', (req, res) => {
+  const _id = req.params._id // get _id from request params
+  const description = req.body.description.trim() // get username and trim space from start and end from request body
+  const duration = req.body.duration // get duration from request body
+  const dateString = req.body.date.trim() // get date string and trim space from start and end from request body
+  const exerciseObject = { description: description, duration: duration } // create a temporary object to contain required attributes
+  if (isValidDate(dateString)) {
+    exerciseObject.date = new Date(dateString) // add the date field to the temporary object
+  }
+  const exercise = new Exercise(exerciseObject) // use the temporary object to create an exercise
+  exercise.save((err, createdExercise) => { // save to database
+    if (err) {
+      res.json({error: err.message })
+    } else {
+      User.findById(_id, (err, user) => { // get the user to get the id and username to be embedded in response
+        if (err) {
+          res.json({error: err.message })
+        } else {
+          user.log.push(createdExercise) // add the newly created exercise to array of exercises
+          user.save((err, updatedUser) => {
+            if (err) {
+              res.json({error: err.message })
+            } else {
+                res.json({ // return the necessary attributes
+                  username: updatedUser.username,
+                  description: createdExercise.description,
+                  duration: createdExercise.duration,
+                  date: createdExercise.date.toDateString(),
+                  _id: updatedUser.id
+                })
+            }
+          })
+        }
+      })
+    }
+  })
+}) 
 
 
 
